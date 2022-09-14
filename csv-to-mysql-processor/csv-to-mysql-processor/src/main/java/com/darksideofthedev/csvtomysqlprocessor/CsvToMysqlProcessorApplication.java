e2c6bccbf4eaa18609182daa50480a002d946359
davidokun <davidokun99@gmail.com>
2022-09-14 21:32:14 +0200

    Initial project commit

---
+++ csv-to-mysql-processor/csv-to-mysql-processor/src/main/java/com/darksideofthedev/csvtomysqlprocessor/CsvToMysqlProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.darksideofthedev.csvtomysqlprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvToMysqlProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvToMysqlProcessorApplication.class, args);
+    }
+
+}
